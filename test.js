bc67a54410d0aaa3f7dbcc1e6f747fc2a53cd4f9
Aryan-eng <aryanagarwal132@gmail.com>
2022-04-17 15:47:13 -0400

    init

--- test.js
+++ test.js
@@ -1,2 +1,3 @@
 var userName = window.prompt("Enter your name:")
 userName? console.log(`Hello${userName}. Welcome to DoNet.`) : console.log('Hello. Welcome to DoNet')
+let account = window.prompt("Enter your email address")
