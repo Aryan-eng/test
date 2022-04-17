9540425972e5cceb4a6bb72c7ce4cfdd38577514
Aryan-eng <72766369+Aryan-eng@users.noreply.github.com>
2022-04-17 16:15:28 -0400

    random number

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 var userName = window.prompt("Enter your name:")
 userName? console.log(`Hello${userName}. Welcome to DoNet.`) : console.log('Hello. Welcome to DoNet')
 let account = window.prompt("Enter your email address")
+console.log(Math.floor(Math.random()*10))
